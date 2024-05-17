785d4228e2dcad8b03b254b4e81617febb13acce
Fedor Tarasenko <fedortarasenko2001@gmail.com>
2024-05-17 12:38:10 +0200

    first commit with cypress

--- vite.config.js
+++ vite.config.js
@@ -4,10 +4,9 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './',
-  test: {
-	environment: 'jsdom',
-	globals: true,
-	setupFiles: './tests/setup.js'
+  baseUrl: './',
+  server: {
+    host: true,
+    port: 8888
   }
 })
