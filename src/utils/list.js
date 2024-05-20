1eadc8ce6134f159d8887a32b139e10c9477770f
Fedor Tarasenko <fedortarasenko2001@gmail.com>
2024-05-20 16:50:31 +0200

    first unit test done

--- src/utils/list.js
+++ src/utils/list.js
@@ -12,10 +12,11 @@ function splitTodosIntoDays(todos) {
 	return [må, ti, on, to, fr, lö, sö]
 }
 
+
+export { splitTodosIntoDays }
+
 // Tips! Du kan få användning för funktioner som:
 // + kopierar en lista och byter plats på två element (snooze)
 // + lägger till ett element på en viss plats i en lista
 // https://www.w3schools.com/jsref/jsref_splice.asp
 // https://www.freecodecamp.org/news/javascript-splice-how-to-use-the-splice-js-array-method/
-
-export { splitTodosIntoDays }
